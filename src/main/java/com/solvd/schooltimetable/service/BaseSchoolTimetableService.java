aeb052688bb9916346706e997d0f3baf673c2310
amuravski <arseniymv@gmail.com>
2022-09-30 15:12:09 +0300

    Base service interface added

---
+++ src/main/java/com/solvd/schooltimetable/service/BaseSchoolTimetableService.java
@@ -0,0 +1,19 @@
+package com.solvd.schooltimetable.service;
+
+import java.util.List;
+
+public interface BaseSchoolTimetableService<T> {
+
+    void create(T toCreate);
+
+    List<T> findAll();
+
+    T findById(Long id);
+
+    void update(T toUpdate);
+
+    void delete(T toDelete);
+
+    void deleteById(Long id);
+
+}
