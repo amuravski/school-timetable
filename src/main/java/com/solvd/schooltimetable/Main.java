0a191271a3c5ff2261404e17043374827a1f3f26
knovash <novash.ki@gmail.com>
2022-10-09 13:23:43 +0300

    added tostring for schooltimetable

--- src/main/java/com/solvd/schooltimetable/Main.java
+++ src/main/java/com/solvd/schooltimetable/Main.java
@@ -2,14 +2,17 @@ package com.solvd.schooltimetable;
 
 import com.solvd.schooltimetable.domain.CalendarDay;
 import com.solvd.schooltimetable.domain.SchoolClass;
+import com.solvd.schooltimetable.domain.SchoolTimetable;
 import com.solvd.schooltimetable.domain.Teacher;
 import com.solvd.schooltimetable.genetic.GeneticAlgo;
 import com.solvd.schooltimetable.genetic.GeneticAlgoConfig;
 import com.solvd.schooltimetable.service.CalendarDayService;
 import com.solvd.schooltimetable.service.SchoolClassService;
+import com.solvd.schooltimetable.service.SchoolTimetableService;
 import com.solvd.schooltimetable.service.TeacherService;
 import com.solvd.schooltimetable.service.impl.CalendarDayServiceImpl;
 import com.solvd.schooltimetable.service.impl.SchoolClassServiceImpl;
+import com.solvd.schooltimetable.service.impl.SchoolTimetableServiceImpl;
 import com.solvd.schooltimetable.service.impl.TeacherServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,7 +33,16 @@ public class Main {
         List<SchoolClass> schoolClasses = schoolClassService.findAll();
         List<Teacher> teachers = teacherService.findAll();
         List<CalendarDay> calendarDays = calendarDayService.findAll();
+        ///
+        SchoolTimetableService schoolTimetableService = new SchoolTimetableServiceImpl();
+        SchoolTimetable st1 = schoolTimetableService.findById(1L);
+        System.out.println(st1);
 
+        SchoolTimetable st2 = schoolTimetableService.findById(2L);
+        System.out.println(st2);
+
+        System.exit(0);
+        ////
         GeneticAlgoConfig geneticAlgoConfig = new GeneticAlgoConfig("geneticAlgoConfig.properties");
         GeneticAlgo geneticAlgo = new GeneticAlgo(geneticAlgoConfig);
         geneticAlgo.setSchoolClasses(schoolClasses);
@@ -42,7 +54,7 @@ public class Main {
         if (geneticAlgoConfig.getMinLessons() * schoolClasses.size() > teachers.size()) {
             throw new RuntimeException("Not enough teachers for this number of classes.");
         }
-        int n = 100;
+        int n = 5;
         long t = System.currentTimeMillis();
         AtomicInteger ideals = new AtomicInteger(0);
         LOGGER.info(IntStream.range(0, n).parallel().boxed()
